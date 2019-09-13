ea4d0f53867a6ae90713eb7c62a91b6dc94025b8
James Rodgers <jamesrodgers@Jamess-MacBook-Air.local>
2019-09-12 21:20:12 -0600

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and Running!");
+//cards for game
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen"
+var cardFour = "king";
+
+console.log("User Flipped " + cardOne);
+console.log("User Flipped " + cardTwo);
