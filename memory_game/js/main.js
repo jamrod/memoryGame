63e907a5d4897cf28ae37172c0c28c5e5fa86886
Jamrod <myfavoritenoisemaker@gmail.com>
2019-09-15 13:48:27 -0600

    Created Game Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
 console.log("Up and Running!");
 //cards for game
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png",
+	},
+	{
+		rank: "queen",
+		suit: 'hearts',
+		cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png",
+	}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -13,10 +35,13 @@ function checkForMatch() {
 	};
 }
 function flipCard(cardID) {
-console.log("User flipped " + cards[cardID]);
-cardsInPlay.push(cardID);
+console.log("User flipped " + cards[cardID].rank);
+console.log("User flipped " + cards[cardID].suit);
+console.log("User flipped " + cards[cardID].cardImage);
+cardsInPlay.push(cards[cardID].name);
 checkForMatch();
 }
 
 flipCard(0);
 flipCard(2);
+
