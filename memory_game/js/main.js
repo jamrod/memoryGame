4a3882ff24a433a4610828e506fc878e1ad9554e
James Rodgers <jamesrodgers@Jamess-MacBook-Air.local>
2019-09-15 09:54:57 -0600

    Created Game Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,19 @@
 console.log("Up and Running!");
 //cards for game
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen"
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo)
+console.log("User Flipped " + cardsInPlay[0]);
+console.log("User Flipped " + cardsInPlay[0]);
 
-console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match.");
+	} else {
+		alert("Sorry, try again.")
+	}
+};
+
